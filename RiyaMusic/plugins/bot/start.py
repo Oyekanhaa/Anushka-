a3ce34a3c77ba02835e36bb584aa646beba50f64
𝐇𝐀𝐂𝐊𝐄𝐑 <ayushprincekmr03@gmail.com>
2026-02-10 13:06:44 +0530

    Update start.py

--- RiyaMusic/plugins/bot/start.py
+++ RiyaMusic/plugins/bot/start.py
@@ -20,8 +20,8 @@ from config import BANNED_USERS
 from strings import get_string
 
 EFFECT_ID = [
-    5107584321108051014,
-    5107584321108051014,
+    5104841245755180586,
+    5104841245755180586,
 ]
 
 
@@ -158,6 +158,7 @@ async def welcome(client, message: Message):
                 await message.stop_propagation()
         except Exception as ex:
             print(ex)
+
 
 
 
