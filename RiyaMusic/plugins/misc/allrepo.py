661d1d6b6cd10f357b33a9e9f819633120bc26b8
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2026-02-10 14:54:01 +0530

    Update allrepo.py

--- RiyaMusic/plugins/misc/allrepo.py
+++ RiyaMusic/plugins/misc/allrepo.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 import requests
-from PURVIMUSIC import app
+from RiyaMusic import app
 
 # Function to chunk the repository info into smaller parts
 def chunk_string(text, chunk_size):
